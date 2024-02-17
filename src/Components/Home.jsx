cadd3e97eafc82f40a6ed124bc314b4a67a9359a
PLTA Gunerathne <152690036+Ama-20913@users.noreply.github.com>
2024-02-17 09:49:15 +0530

    add all changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/circles.png";
+import image from "../images/Green.jpg";
 
 const imageAltText = "Web Developer and Data Science Undergrade. This is background photo";
 
